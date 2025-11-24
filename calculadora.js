2f20847daa0bd216051f751b222c07a91408fd5c
VitorM29 <vitorqsmatos@gmail.com>
2025-11-24 20:12:43 -0300

    Enhance calculator with limit and delete features
    Added character limit and delete functionality to calculator.

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,33 @@
 let form = document.querySelector(".form");
 let calc = "";
 let result = "";
+const lim = 10;
 let htmlResult = document.querySelector(".htmlResult");
-form.addEventListener("submit", (event)=>{
+
+form.addEventListener("submit", (event) => {
     event.preventDefault()
-    
+
     let submitter = event.submitter.name
-    
-    if(submitter == "="){
+
+    if (submitter == "=") {
         result = eval(calc);
+        if (String(result).length > lim) {
+            result = String(result).slice(0, lim);
+        }
         htmlResult.innerHTML = `${result}`
-    } else if(submitter == "C"){
+    } else if (submitter == "C") {
         calc = " ";
         htmlResult.innerHTML = `${calc}`
+    } else if (submitter == "del") {
+        calc = calc.slice(0, -1);
+        htmlResult.innerHTML = `${calc}`
+    } else if (submitter == "%") {
+        calc = calc + "/100*";
+        htmlResult.innerHTML = calc;
+    } else if (calc.length > lim) {
+        alert("EXCEDEU O LIMITE DE CARACTERES")
     } else{
         calc = calc + submitter;
         htmlResult.innerHTML = `${calc}`
     }
-    
-
 })
